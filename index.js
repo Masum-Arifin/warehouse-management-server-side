39809aebc364a057f98f1cbc4199ecf1a87a2dc1
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-08 23:53:15 +0600

    new code

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.use(express.json());
 
 
 // verifyJWT
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'Unauthorized access' })
-//     }
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
